0c442f3dc0c2626c01f2b068c90b85f85c3cc99a
mohamed20medhat <egailmohamed2018@gmail.com>
2022-03-19 13:07:02 +0200

    designed the initial frontend

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const app = express()
+const port = process.env.port || 3000
+const path = require('path')
+
+// middlewares
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "/public"))); // to serve static files
+
+
+//routes
+app.use('/', require('./router/root'))
+
+
+// handeling trying to acces non exisiting pages
+app.all("*", (req, res) => {
+    res.status(404);
+    if (req.accepts("html")) {
+        res.sendFile(path.join(__dirname, "views", "404.html"));
+    } else if (req.accepts("json")) {
+        res.json({ error: "404 Not Found" });
+    } else {
+        res.type("txt").send("404 Not Found");
+    }
+});
+
+app.listen(port, () => {
+    console.log(`server runing on port ${port}`)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
