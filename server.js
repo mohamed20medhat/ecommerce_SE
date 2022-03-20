fccb7bd14afabaacb0d79d4cc1d7fbe04f65d710
Mostafa Ahmed <79556633+mosta1489@users.noreply.github.com>
2022-03-20 15:23:09 +0200

    this is test commit

--- server.js
+++ server.js
@@ -1,58 +1,40 @@
-const express = require('express')
-const app = express()
-const port = process.env.port || 3000
-const path = require('path')
+const express = require("express");
+const app = express();
+const port = process.env.port || 3000;
+const path = require("path");
 const mongoose = require("mongoose");
 const connectDB = require("./config/dbConn");
 
 //connect to mongodb
 connectDB();
 
-
 // middlewares
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "/public"))); // to serve static files
 
-
 //routes
-app.use('/', require('./router/root'))
-
+app.use("/", require("./router/root"));
 
 // handeling trying to acces non exisiting pages
 app.all("*", (req, res) => {
-    res.status(404);
-    if (req.accepts("html")) {
-        res.sendFile(path.join(__dirname, "views", "404.html"));
-    } else if (req.accepts("json")) {
-        res.json({ error: "404 Not Found" });
-    } else {
-        res.type("txt").send("404 Not Found");
-    }
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
 });
 
+// -----------------------------------v
+// this commit from mostafa for test
+
+console.log("this is our team");
+
+// -----------------------------------
+
 app.listen(port, () => {
-    console.log(`server runing on port ${port}`)
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  console.log(`server runing on port ${port}`);
+});
