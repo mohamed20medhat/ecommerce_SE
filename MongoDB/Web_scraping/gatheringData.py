6bfaacf82fb3d28be61f4c5e5321d197dbbb50f9
Mohamed Hassan <zmlkawy67@gmail.com>
2022-04-10 18:47:19 +0200

    modify gathering and store data

--- MongoDB/Web_scraping/gatheringData.py
+++ MongoDB/Web_scraping/gatheringData.py
@@ -1,73 +1,82 @@
 import requests
 from bs4 import BeautifulSoup
+import os
+from PIL import Image
+import io
 
-
-mens_bags = 'https://www.jumia.com.eg/bags/?page='       
-
-Beauty_health = 'https://www.jumia.com.eg/body-skin-care/?page='        
- 
-labtops = 'https://www.jumia.com.eg/laptops/?page='       
-
-Smart_phones = 'https://www.jumia.com.eg/smartphones/?page='    
 
 babies = 'https://www.jumia.com.eg/mlp-free-shipping/baby-products/?page='  
 
 
-products_collection = []
-
-def Making_Data_of_products(url):
+def Getting_Data(url):
     
+    print(f"\n{'*' * 10}  Products of {url.split('/')[-2]}  {'*' * 10}\n")
+    # Making a folder to save images
+    folder_name = 'Images'
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
     li = []
-    for i in range(1, 6):   # getting data from 5 pages.
+        
+        
+    for i in range(1, 6):   # getting data from 5 pages of each type of products.
         response = requests.get(url + str(i))
         soup = BeautifulSoup(response.content, 'lxml')
-
         data = soup.find('div', {'class': '-paxs row _no-g _4cl-3cm-shs'})
         products = data.find_all('article', {'class': 'prd _fb col c-prd'})
         
+        x = 0
         for pr in products: 
+        
             title = pr.find('a').find('div', {'class': 'info'}).find('h3').text
-            img   = pr.find('a').find('div', {'class': 'img-c'}).find('img', {'class': 'img'})['data-src']
+            imgLink   = pr.find('a').find('div', {'class': 'img-c'}).find('img', {'class': 'img'})['data-src']
             price = float(pr.find('a').find('div', {'class': 'info'}).find('div', {'class': 'prc'}).text.split()[1].replace(',', ''))
             brand = pr.find('a')['data-brand']
             tags  = pr.find('a')['data-category'].split('/')
-
+            
+            img_data = requests.get(imgLink).content
+            try:
+                
+                # path of saving the photos
+                path_ = r"{}.jpg".format(os.path.join(folder_name, f"{title.split('-')[0].strip().split('/')[0]}")) 
+                print('>>>>', path_.split('\\')[1][:-4])
+                
+                # saving photos in the last path.
+                with open(path_, mode='wb') as handler:
+                    handler.write(img_data)
+                            
+                # converting photos to bytes to save it in mongodb.
+                im = Image.open(path_) 
+                image_bytes = io.BytesIO()
+                im.save(image_bytes, format='JPEG')
+                li.append(
+                    {
+                        'title': title,
+                        'price': price,
+                        'brand': brand,
+                        'image': image_bytes.getvalue(),
+                        'category': tags[0],
+                        'tags': tags,
+                        'total_rate': 0,
+                        'count_of_ratings': 0,
+                        'reviews': []
+                    }
+            )
+            except:   # if there are any not allowed symbol in the title to use in the path. 
+                continue 
+   
+   
+   
+            # # if we need to add the original scrapped ratings.
             # rating = float(pr.find('a').find('div', {'class': 'info'}).find('div', {'class': 'rev'}).text.split()[0])
             # no_of_rev  = int(pr.find('a').find('div', {'class': 'info'}).find('div', {'class': 'rev'}).text.split()[-1][2:4])
-
-            li.append(
-                {
-                    'title': title,
-                    'price': price,
-                    'brand': brand,
-                    'image': img,
-                    'category': tags[0],
-                    'tags': tags,
-                    'total_rate': 0,
-                    'count_of_ratings': 0,
-                    'reviews': []
-                }
-            )
-            
+    print('=' * 50)
     return li
+
+def testing():
+    # data of the baby products
+    Baby_products = Getting_Data(babies)
     
-# data of the baby products
-Baby_products = Making_Data_of_products(babies)
-
-# data of the smart phones 
-SmartPhones_products = Making_Data_of_products(Smart_phones)
-
-# data of the Beauty and helthe products.
-BeautyHealth_products = Making_Data_of_products(Beauty_health)
-
-# data of the labtops.
-Labtops_products = Making_Data_of_products(labtops)
-
-# data of the mens bags.
-MensBagsProducts = Making_Data_of_products(mens_bags)
-
-
-
-
-# just testing
-print(Baby_products[0], '\n\n\n', SmartPhones_products[0], '\n\n\n',BeautyHealth_products[0], '\n\n\n',Labtops_products[0], '\n\n\n', MensBagsProducts[0])
+    print('\n\nDone', '\n\n', Baby_products[4],'\n\n')
+if __name__ == "__main__":
+    testing()
+    
