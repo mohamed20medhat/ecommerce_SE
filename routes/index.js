e5c5846ef636c9e944a71995ca5e95d2dfbb0491
Mostafa Ahmed <79556633+mosta1489@users.noreply.github.com>
2022-04-13 06:17:25 +0200

    check cookies befor open any page

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,23 @@
-const express = require('express')
-const router = express.Router()
-const Product = require('../models/product')
+const express = require("express");
+const router = express.Router();
+const Product = require("../models/product");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+router.use(cookieParser());
 
-router.get('/', async (req, res) => {
-  let products 
+router.get("/", async (req, res) => {
+  if (!req.cookies.jwt) {
+    return res.redirect("/login");
+  }
+  let products;
   try {
     products = await Product.find().sort({ createAt: "desc" }).limit(15).exec();
   } catch {
     products = [];
   }
   res.render("index", { products: products });
-})
+});
 
-module.exports = router
+module.exports = router;
 
-// added a new commit 
+// added a new commit
