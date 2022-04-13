903b3311b5b53202150b9865b6fd47d6ece419f4
Mostafa Ahmed <79556633+mosta1489@users.noreply.github.com>
2022-04-13 03:27:11 +0200

    check cookie befor render pages

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,24 @@
-const express = require('express')
-const router = express.Router()
-const Product = require('../models/product')
+const express = require("express");
+const router = express.Router();
+const Product = require("../models/product");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+router.use(cookieParser());
+const checkCookies = require("./checkCookies");
 
-router.get('/', async (req, res) => {
-  let products 
+router.get("/", async (req, res) => {
+  if (!req.cookies.jwt) {
+    return res.redirect("/login");
+  }
+  let products;
   try {
     products = await Product.find().sort({ createAt: "desc" }).limit(10).exec();
   } catch {
     products = [];
   }
   res.render("index", { products: products });
-})
+});
 
-module.exports = router
+module.exports = router;
 
-// added a new commit 
+// added a new commit
--- routes/products.js
+++ routes/products.js
@@ -2,128 +2,129 @@ const express = require("express");
 const router = express.Router();
 const Product = require("../models/product");
 const imageMimeTypes = ["image/jpeg", "image/png", "images/gif", "image/jpg"];
-
-
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+router.use(cookieParser());
 
 // All products route
 router.get("/", async (req, res) => {
-    let query = Product.find();
-    if (req.query.name != null && req.query.name != "") {
-        query = query.regex("name", new RegExp(req.query.name, "i"));
-    }
+  if (!req.cookies.jwt) {
+    return res.redirect("/login");
+  }
 
-    try {
-        const products = await query.exec();
-        res.render("products/index", {
-            products: products,
-            searchOptions: req.query,
-        });
-    } catch {
-        res.redirect("/");
-    }
+  let query = Product.find();
+  if (req.query.name != null && req.query.name != "") {
+    query = query.regex("name", new RegExp(req.query.name, "i"));
+  }
+
+  try {
+    const products = await query.exec();
+    res.render("products/index", {
+      products: products,
+      searchOptions: req.query,
+    });
+  } catch {
+    res.redirect("/");
+  }
 });
-
 
 // new product
 router.get("/new", async (req, res) => {
-    renderNewPage(res, new Product());
-});
+  if (!req.cookies.jwt) {
+    return res.redirect("/login");
+  } else {
+    const user = jwt.verify(req.cookies.jwt, "secret").username;
+    if (user !== "admin") {
+      return res.redirect("/");
+    }
+  }
 
+  renderNewPage(res, new Product());
+});
 
 // create new product
 router.post("/", async (req, res) => {
-    const product = new Product({
-        name: req.body.name,
-        description: req.body.description,
-        rating: req.body.rating,
-    });
+  const product = new Product({
+    name: req.body.name,
+    description: req.body.description,
+    rating: req.body.rating,
+  });
 
-    saveCover(product, req.body.cover);
+  saveCover(product, req.body.cover);
 
-    try {
-        const newProduct = await product.save();
-        res.redirect(`products/${newProduct.id}`);
-    } catch {
-        renderNewPage(res, product, true);
-    }
+  try {
+    const newProduct = await product.save();
+    res.redirect(`products/${newProduct.id}`);
+  } catch {
+    renderNewPage(res, product, true);
+  }
 });
-
 
 //show a certain product
 router.get("/:id", async (req, res) => {
-    try {
-        const product = await Product.findById(req.params.id)
-            .exec();
-        res.render("products/show", { product: product });
-    } catch {
-        res.redirect("/");
-    }
+  try {
+    const product = await Product.findById(req.params.id).exec();
+    res.render("products/show", { product: product });
+  } catch {
+    res.redirect("/");
+  }
 });
-
 
 // delete Product route
 router.delete("/:id", async (req, res) => {
-    let product;
-    try {
-        product = await Product.findById(req.params.id);
-        await product.remove();
-        res.redirect("/products");
-    } catch {
-        if (product != null) {
-            res.render("products/show", {
-                product: product,
-                errorMessage: "could not remove product",
-            });
-        } else {
-            res.redirect("/");
-        }
+  const user = jwt.verify(req.cookies.jwt, "secret").username;
+  if (user !== "admin") {
+    return res.redirect("/");
+  }
+
+  let product;
+  try {
+    product = await Product.findById(req.params.id);
+    await product.remove();
+    res.redirect("/products");
+  } catch {
+    if (product != null) {
+      res.render("products/show", {
+        product: product,
+        errorMessage: "could not remove product",
+      });
+    } else {
+      res.redirect("/");
     }
+  }
 });
 
-
 async function renderFormPage(res, product, form, hasError = false) {
-    try {
-        const params = {
-            product: product,
-        };
+  try {
+    const params = {
+      product: product,
+    };
 
-        if (hasError) {
-            if (form === "edit") {
-                params.errorMessage = "Error Editing Book";
-            } else {
-                params.errorMessage = "Error Updating Book";
-            }
-        }
-
-        res.render(`products/${form}`, params);
-    } catch {
-        res.redirect("/products");
+    if (hasError) {
+      if (form === "edit") {
+        params.errorMessage = "Error Editing Book";
+      } else {
+        params.errorMessage = "Error Updating Book";
+      }
     }
+
+    res.render(`products/${form}`, params);
+  } catch {
+    res.redirect("/products");
+  }
 }
-
-
-
 
 async function renderNewPage(res, product, hasError = false) {
-    renderFormPage(res, product, "new", hasError);
+  renderFormPage(res, product, "new", hasError);
 }
 
-
-
-
-
 function saveCover(product, coverEncoded) {
-    if (coverEncoded == null) return;
-    const cover = JSON.parse(coverEncoded);
-    if (cover != null && imageMimeTypes.includes(cover.type)) {
-        product.coverImage = new Buffer.from(cover.data, "base64");
-        product.coverImageType = cover.type;
-    }
+  if (coverEncoded == null) return;
+  const cover = JSON.parse(coverEncoded);
+  if (cover != null && imageMimeTypes.includes(cover.type)) {
+    product.coverImage = new Buffer.from(cover.data, "base64");
+    product.coverImageType = cover.type;
+  }
 }
 
 module.exports = router;
-
-
-
-
-
