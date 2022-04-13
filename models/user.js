9a115ac304073fe4d52f52cb0afa7785bd15c503
Mostafa Ahmed <79556633+mosta1489@users.noreply.github.com>
2022-04-13 03:09:26 +0200

    not actual change in code

--- models/user.js
+++ models/user.js
@@ -2,9 +2,13 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    username: {
+      type: String,
+      unique: true,
+    },
+    name: String,
+    password: String,
+    cart: Array,
   },
   { collection: "users" }
 );
